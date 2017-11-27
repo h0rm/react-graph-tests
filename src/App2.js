d91912b3a258fc6f37a48d4895223d55e13a80c5
Manuel Keglevic <manuel.keglevic@gmail.com>
2017-11-27 13:40:52 +0100

    Cleanup

--- src/App2.js
+++ src/App2.js
@@ -1,19 +1,17 @@
-import React from "react";
+import React from 'react';
 
 // Import React Table
 import ReactTable from "react-table";
-import "react-table/react-table.css";
+import 'react-table/react-table.css';
 import update from 'react-addons-update';
 import './App.css';
 import Graph from './Graph';
-// import mydata from './data/convertcsv.json'
-import mydata from './data/processed-torch.csv.json'
-import JSONTree from 'react-json-tree'
+import mydata from './data/processed-torch.csv.json';
+import JSONTree from 'react-json-tree';
 import '../node_modules/react-json-inspector/json-inspector.css';
-// import './react-dropdown-tree-select/src/index.css';
-import DropdownTreeSelect from './react-dropdown-tree-select/src/index'
+import DropdownTreeSelect from './react-dropdown-tree-select/src/index';
 
-let Inspector = require('react-json-inspector')
+let Inspector = require('react-json-inspector');
 
 const JSONtheme = {
   scheme: 'monokai',
@@ -38,30 +36,6 @@ const JSONtheme = {
 
 let _ = require('lodash');
 
-// let data = [
-//       { name: 1, cost: 4.11, impression: 100 },
-//       { name: 2, cost: 2.39, impression: 120 },
-//       { name: 3, cost: 1.37, impression: 150 },
-//       { name: 4, cost: 1.16, impression: 180 },
-//       { name: 5, cost: 2.29, impression: 200 },
-//       { name: 6, cost: 3, impression: 499 },
-//       { name: 7, cost: null, impression: 50 },
-//       { name: 8, cost: 2.52, impression: 100 },
-//       { name: 9, cost: 1.79, impression: 200 },
-//       { name: 10, cost: null, impression: 222},
-//       { name: 11, cost: 4.3, impression: 210 },
-//       { name: 12, cost: 4.41, impression: 300 },
-//       { name: 13, cost: 2.1, impression: 50 },
-//       { name: 14, cost: 8, impression: 190 },
-//       { name: 15, cost: 0, impression: 300 },
-//       { name: 16, cost: 9, impression: 400 },
-//       { name: 17, cost: 3, impression: 200 },
-//       { name: 18, cost: 2, impression: 50 },
-//       { name: 19, cost: 3, impression: 100 },
-//       { name: 20, cost: 7, impression: 100 }
-//     ];
-
-let graph_data = require('./data/02d1f904-4b87-4b43-cbaa-d2aba9b52a06.json')
 const isObject = (item) => {
       return (item && typeof item === 'object' && !Array.isArray(item));
     }
